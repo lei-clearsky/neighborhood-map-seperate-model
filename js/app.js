fec436ec90df25061751c6185d676c6348f04e84
lei-clearsky <sportzhulei@gmail.com>
2014-12-13 19:21:13 -0500

    attempt to display map

--- js/app.js
+++ js/app.js
@@ -1,8 +1,37 @@
 function MapViewModel() {
 
 	var self = this;
+	var map; 
+	var mapOptions;
+	var venuesOnMap = [];
+	var VenuesOnList = [];
+	var venueLat;
+	var venueLon;
+	var venueName;
+	var bounds = window.mapBounds;
+	var service;
+	var marker;
+	var infoWinder;
+	var defaultExploreKeyword = 'bestnearby';
+	var defaultNeighborhood = 'new york';
 
 	self.exploreKeyword = ko.observable('');
 	self.neighborhood = ko.observable('');
+	self.topPicks = ko.observableArray('');
 
+	// function that initializes the map
+	function initializeMap() {
+		mapOptions = {
+			disableDefaultUI: true,
+			zoom: 15
+		};
+		map = new google.maps.Map(document.querySelector('#myMap'), mapOptions);
+	};
+
+	initializeMap();
 };
+
+// initialize the MapViewModel binding
+$(function() {
+  ko.applyBindings(new MapViewModel());
+});
