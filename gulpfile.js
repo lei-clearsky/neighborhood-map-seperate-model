27d6dd25e89b173d4349f99b74744fb12a8b1944
lei-clearsky <sportzhulei@gmail.com>
2015-02-23 14:14:10 -0500

    minor updates on async files

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var csso = require('gulp-csso');
 var concat = require('gulp-concat');
 
 gulp.task('compressJS', function() {
-	gulp.src('js/*.js')
+	gulp.src(['js/knockout-3.2.0.js', 'js/bootstrap.js', 'js/jquery.swipebox.js', 'js/skycons.js', 'js/app.js'])
 	.pipe(concat('app.min.js'))
 	.pipe(uglify())
 	.pipe(gulp.dest('build'));
